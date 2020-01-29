c0b2fa0bdcc1ff391428bf1d7475e859df7ca97b
Melih Kaçaman <mkacamann@gmail.com>
2020-01-29 23:42:07 +0300

    top container has been done.

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String lessonName = "";
-
+  int credit;
+  double letterPoint = 4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text("Not Hesaplama"),
       ),
@@ -34,7 +36,7 @@ class _MyAppState extends State<MyApp> {
         children: <Widget>[
           Expanded(
             child: Container(
-              color: Colors.orangeAccent,
+              color: Colors.pink.shade200,
               child: Form(
                 child: Column(
                   children: <Widget>[
@@ -42,6 +44,12 @@ class _MyAppState extends State<MyApp> {
                       decoration: InputDecoration(
                         labelText: "Ders Adı",
                         hintText: "Ders adını giriniz...",
+                        hintStyle: TextStyle(fontSize: 18),
+                        labelStyle: TextStyle(fontSize: 21),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.purple, width: 2),
+                        ),
                         border: OutlineInputBorder(),
                       ),
                       validator: (value) {
@@ -55,6 +63,51 @@ class _MyAppState extends State<MyApp> {
                         lessonName = value;
                       },
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          child: DropdownButton(
+                            items: getCreditsItems(),
+                            value: credit,
+                            onChanged: (value) {
+                              setState(() {
+                                credit = value;
+                              });
+                            },
+                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                          margin: EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: DropdownButton(
+                            items: getLetterPointsItems(),
+                            value: letterPoint,
+                            onChanged: (value) {
+                              setState(() {
+                                letterPoint = value;
+                              });
+                            },
+                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                          margin: EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
@@ -62,12 +115,89 @@ class _MyAppState extends State<MyApp> {
           ),
           Expanded(
             child: Container(
-              color: Colors.blue,
+              color: Colors.blue.shade300,
               child: Text("List"),
             ),
           )
         ],
       ),
     );
+  }
+
+  List<DropdownMenuItem<int>> getCreditsItems() {
+    List<DropdownMenuItem<int>> items = List.generate(
+      10,
+      (index) => DropdownMenuItem<int>(
+        child: Text(
+          index.toString() + " Kredi",
+          style: TextStyle(fontSize: 20),
+        ),
+        value: index,
+      ),
+    );
+
+    return items;
+  }
+
+  List<DropdownMenuItem<double>> getLetterPointsItems() {
+    List<DropdownMenuItem<double>> harfler = [];
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " AA ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 4,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " BA ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 3.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " BB ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 3,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " CB ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 2.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " CC ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 2,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " DC ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 1.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " DD ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 1,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " FF ",
+        style: TextStyle(fontSize: 20),
+      ),
+      value: 0,
+    ));
+
+    return harfler;
   }
 }
